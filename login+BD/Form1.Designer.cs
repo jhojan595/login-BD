32a9a3c2cb041e7a2bd51f05f4677780e93e8498
Jhojan <jhojanandres.espinosata@ierepublicadehonduras.edu.co>
2023-10-20 20:52:50 -0500

    Agregar archivos de proyecto.

---
+++ login+BD/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace login_BD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.ContraseñaTextBox = new System.Windows.Forms.TextBox();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.BackColor = System.Drawing.Color.Peru;
+            this.BtnIniciar.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIniciar.Location = new System.Drawing.Point(358, 193);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(87, 38);
+            this.BtnIniciar.TabIndex = 0;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.UseVisualStyleBackColor = false;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // ContraseñaTextBox
+            // 
+            this.ContraseñaTextBox.Location = new System.Drawing.Point(327, 109);
+            this.ContraseñaTextBox.Name = "ContraseñaTextBox";
+            this.ContraseñaTextBox.Size = new System.Drawing.Size(171, 20);
+            this.ContraseñaTextBox.TabIndex = 1;
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(327, 61);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(171, 20);
+            this.NombreTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(221, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.ContraseñaTextBox);
+            this.Controls.Add(this.BtnIniciar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.TextBox ContraseñaTextBox;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ login+BD/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BCrypt.Net;
+
+
+namespace login_BD
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-R4TG502;Initial Catalog=DB_login_DB1;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            string nombre = NombreTextBox.Text;
+            string contraseña = ContraseñaTextBox.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT id, contraseña, bloqueada, salt FROM tbl_user WHERE nombre = @Nombre";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            int userId = reader.GetInt32(0);
+                            string hashedPassword = reader.GetString(1);
+                            bool bloqueada = reader.GetBoolean(2);
+                            string salt = reader.GetString(3);
+
+                            if (bloqueada)
+                            {
+                                MessageBox.Show("La cuenta está bloqueada. Comunícate con el soporte.");
+                            }
+                            else
+                            {
+                                if (BCrypt.Net.BCrypt.Verify(contraseña, hashedPassword))
+                                {
+                                    MessageBox.Show("Inicio de sesión exitoso");
+                                    // Realiza acciones adicionales tras un inicio de sesión exitoso.
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Contraseña incorrecta.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Usuario no encontrado");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
